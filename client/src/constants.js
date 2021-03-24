692d2ff840d325a7d92846fd41f3b1a24f7e81ee
leonardodelira <leonardo.lira@hotmail.com.br>
2021-03-24 20:56:20 -0300

    live 1 - criando interfaces com Blessed e padrao Builder

---
+++ client/src/constants.js
@@ -0,0 +1,14 @@
+export const constants = {
+  events: {
+    app: {
+      MESSAGE_SENT: 'message:set',
+      MESSAGE_RECEIVED: 'message:received',
+      ACTIVITYLOG_UPDATED: 'activityLog:updated',
+      STATUS_UPDATED: 'status:updated'
+    },
+
+    socket: {
+
+    }
+  }
+}
---
+++ client/src/terminalController.js
@@ -0,0 +1,93 @@
+import ComponentsBuilder from "./components.js"
+import { constants } from "./constants.js"
+
+export default class TerminalController {
+  #usersColors = new Map()
+
+  constructor() { }
+
+  #pickCollor() {
+    return `#${((1 << 24) * Math.random() | 0).toString(16)}-fg`
+  }
+
+  #getUserCollor(userName) {
+    if (this.#usersColors.has(userName))
+      return this.#usersColors.get(userName)
+
+    const collor = this.#pickCollor()
+    this.#usersColors.set(userName, collor)
+
+    return collor
+  }
+
+  #onInputReceived(eventEmitter) {
+    return function () {
+      const message = this.getValue()
+      console.log(message)
+      this.clearValue()
+    }
+  }
+
+  #onMessageReceived({ screen, chat }) {
+    return msg => {
+      const { userName, message } = msg
+      const collor = this.#getUserCollor(userName)
+
+      chat.addItem(`{${collor}}{bold}${userName}{/}: ${message}`)
+      screen.render()
+    }
+  }
+
+  #onLogChanged({ screen, activityLog }) {
+    return msg => {
+      const [userName] = msg.split(/\s/)
+      const collor = this.#getUserCollor(userName)
+      activityLog.addItem(`{${collor}}{bold}${msg.toString()}{/}`)
+      screen.render()
+    }
+  }
+
+  #onStatusChanged({ screen, status }) {
+    return users => {
+      const { content } = status.items.shift()
+      status.clearItems()
+      status.addItem(content)
+
+      users.forEach(userName => {
+        const collor = this.#getUserCollor(userName)
+        status.addItem(`{${collor}}{bold}${userName.toString()}{/}`)
+      })
+
+      screen.render()
+    }
+  }
+
+  #registerEvents(eventEmitter, components) {
+    eventEmitter.on(constants.events.app.MESSAGE_RECEIVED, this.#onMessageReceived(components))
+    eventEmitter.on(constants.events.app.ACTIVITYLOG_UPDATED, this.#onLogChanged(components))
+    eventEmitter.on(constants.events.app.STATUS_UPDATED, this.#onStatusChanged(components))
+  }
+
+  async initializeTable(eventEmitter) {
+    const components = new ComponentsBuilder()
+      .setScreen({ title: 'HackerChat - deLira ' })
+      .setLayoutComponent()
+      .setInputComponent(this.#onInputReceived(eventEmitter))
+      .setChatComponent()
+      .setActivityLogComponent()
+      .setStatusComponent()
+      .build()
+
+    this.#registerEvents(eventEmitter, components)
+
+    components.input.focus()
+    components.screen.render()
+
+    setInterval(() => {
+      const users = ['delira']
+      eventEmitter.emit(constants.events.app.STATUS_UPDATED, users)
+      users.push('lari')
+      eventEmitter.emit(constants.events.app.STATUS_UPDATED, users)
+    }, 1500)
+  }
+}
